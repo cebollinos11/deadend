5b954cdeada6d9afa42f639aa857bd47aad29016
cebollinos11 <cebollinos@gmail.com>
2016-08-05 19:33:10 +0200

    hammer guy

---
+++ Assets/FatEnemy.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FatEnemy : Enemy  {
+
+    public float StompDistance;
+    bool hasStomped;
+    [SerializeField]bool waiting;
+    protected override void Start()
+    {
+        
+        base.Start();
+    }
+
+    public void Stop()
+    {
+        anim.ResetTrigger("HammerDown");
+        waiting = true;
+
+        Debug.Log("Called to stop");
+    }
+
+    public void ResumeAttack()
+    {
+        waiting = false;
+        Debug.Log("Called to resume");
+    }
+
+    public void HammerDown()
+    {
+        player.GetComponent<Hero>().cam.PlayBump();
+    }
+
+    protected override void Update()
+    {
+
+        
+
+        if(!waiting)
+            base.Update();
+        //Debug.Log(StompDistance.ToString() + " vs " + transform.position.ToString());
+
+        if(!hasStomped && StompDistance >  transform.position.x)
+        {
+            anim.SetTrigger("HammerDown");
+            Debug.Log("Call to stomp");
+            hasStomped = true;
+            
+
+        }
+        
+	}
+}
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -36,7 +36,8 @@ public class Hero : MonoBehaviour {
     [SerializeField]
     ParticleSystem BloodParticleEmitter;
 
-    CameraHandler cam;
+    [HideInInspector]
+    public CameraHandler cam;
     GameManager gm;
 
     SpriteRenderer sr;
