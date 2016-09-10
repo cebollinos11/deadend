c0b9347d0ea86284c339778cebab0b0f9acf7956
Oscar Losada <oscarlosu@gmail.com>
2016-09-10 15:45:39 +0200

    Start Screen.

---
+++ Assets/Scripts/LoadNextSceneOnButtonDown.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadNextSceneOnButtonDown : MonoBehaviour {
+    public int resetScene = 1;
+    // Use this for initialization
+    void Start () {
+        InputManager.OnButtonDown += HandleButtonDown;
+    }
+
+    private void HandleButtonDown() {
+        SceneManager.LoadScene(resetScene);
+    }
+}
